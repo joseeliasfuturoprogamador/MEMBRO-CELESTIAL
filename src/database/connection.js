86931ed9f8f379ec6c4576fe2548a1de6ebd57a1
JOSE ELIAS SILVA SOUSA <zeliassilva005@gmail.com>
2025-01-05 17:22:51 -0300

    membro celestial

---
+++ src/database/connection.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD
+
+const connect = async () => {
+    try{
+        await mongoose.connect(
+            `mongodb+srv://${dbUser}:${dbPassword}@membros005.0l8jn.mongodb.net/?retryWrites=true&w=majority&appName=MEMBROS005`
+        );
+        console.log("conexão feita com sucesso!");
+    } catch (err) {
+        console.error("Erro ao conectar com o banco de dados", err);
+        process.exit(1);
+    };
+};
+
+module.exports = connect;
