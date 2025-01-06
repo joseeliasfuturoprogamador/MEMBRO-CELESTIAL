7c9dfa8e3a96724bae1f4bb0c9cb31d7d3901d87
JOSE ELIAS SILVA SOUSA <zeliassilva005@gmail.com>
2025-01-06 02:22:29 -0300

    ALTERACAO E IMPLEMENTACAO

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ const connect = require("./database/connection");
 connect();
 const app = express();
 
-app.use(express.json)
+app.use(express.json());
 
 
 app.listen(3000, () => {
-console.log('servidor rodando na porta 3000')
+    console.log('servidor rodando na porta 3000');
 });
---
+++ src/models/UsuarioSchema.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const usuarioSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    nascimento: { type: Number, required: true, unique: true },
+    endereco: { type: String, required: true, },
+    bairro: { type: String, required: true, },
+    filiacao: { type: String, required: true, },
+    estado_civil: { type: String, required: true, },
+    cpf: { type: String, minlength: 11, maxlength: 11, unique: true },
+    area: { type: String, required: true,  },
+    congregacao: { type: String, required: true,  },
+    dirigente: { type: String, required: true,  },
+    convencao: { type: Number, required: true,  },
+    funcao: { type: String, required: true,  },
+    discipulado: { type: String, required: true,  },
+    data_de_batismo: { type: Number, require: true,},
+});
+
+const Usuario  = mongoose.model('Usuario', usuarioSchema);
+
+module.export  = Usuario;
