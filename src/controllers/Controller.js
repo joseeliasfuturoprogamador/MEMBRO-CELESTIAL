54aefb32114c9f257082fc08268c54fcecbf3a94
JOSE ELIAS SILVA SOUSA <zeliassilva005@gmail.com>
2025-01-13 03:03:41 -0300

    conclusao temporaria do controller

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -15,4 +15,49 @@ const criarUsuario = async (req, res) => {
     } catch (error) {
         res.status(400).json({ error: error.message })
     }
+};
+
+const listarUsuarios =  async (req, res) => {
+    try{
+        const users = await userService.listarUsuarios();
+        res.status(200).json(users);
+    } catch (error) {
+     res.status(500).json({message: "Ocorreu um erro ao listar Usuários"});
+    }
+};
+
+const listarPorId = async (req, res) => {
+    try{
+        const users = await userService.listarPorId(req.params.id);
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(404).json({message: "Erro ao listar Usuários pot Id"});
+    }
+};
+
+const AtualizarPorId = async (req, res) => {
+    try{
+ const users = await userServic.AtualizarPorId(req.params.id, req.body);
+ res.status(200).json(users)
+    } catch (error){
+        res.status(404).json({ message: "Erro ao atualizar por Id"});
+    }
+}
+
+const DeletarPorId = async (req, res) => {
+    try{
+        await userService.deletarPorId(req.params.id);
+        return res.status(200).json({message: "Deletado com sucesso"})
+    } catch (error) {
+        res.status(404).json({ message: "Erro ao deletar Usuário"});
+    }
+}
+
+
+module.exports = {
+    criarUsuario,
+    listarUsuarios,
+    listarPorId,
+    AtualizarPorId,
+    DeletarPorId,   
 }
