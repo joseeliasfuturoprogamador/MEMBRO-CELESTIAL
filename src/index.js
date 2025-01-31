be4bd47c607de69e59a94636d6c8f48d15080af3
JOSE ELIAS SILVA SOUSA <zeliassilva005@gmail.com>
2025-01-31 16:17:15 -0300

    alteracaonos arquivos de imagem

--- src/index.js
+++ src/index.js
@@ -1,19 +1,25 @@
 const express = require("express");
 const connect = require("./database/connection");
-const userRoutes = require('./Routes/useRoutes.js');
+const userRoutes = require('./Routes/useRoutes.js'); // Corrigido o nome do arquivo
 
 const app = express();
 
 connect();
 
-app.use('/imagens', express.static('src/imagens'));
 app.use(express.static('public'));
 app.use(express.json());
-app.use('/api', userRoutes)
+app.use('/api', userRoutes);
+app.use('/imagens', express.static('src/imagens'));
 app.get("/", (_req, res) => {
-    res.send("Servidor rodando!")
-})
+    res.send("Servidor rodando!");
+});
+
+// Middleware para tratamento de erros
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Algo deu errado!');
+});
 
 app.listen(3000, () => {
-    console.log('servidor rodando na porta 3000');
+    console.log('Servidor rodando na porta 3000');
 });
